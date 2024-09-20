6f3b1516434d30b02f71ee99e148b4f93ece2467
Bunnygirldaisy <daisyreyvergara@gmail.com>
2024-09-20 21:54:24 +0800

    NewJava

--- src/myfirstjavaproject/Grades.java
+++ src/myfirstjavaproject/Grades.java
@@ -1,37 +1,71 @@
-
 package myfirstjavaproject;
 
 import java.util.Scanner;
 
-
 public class Grades {
-    
-    public void getGrade(){
-      
+     private String id;
+    private String name;
+    private int math, sci, eng, com;
+
+    public void getGrade() {
         Scanner input = new Scanner(System.in);
-                
-        String name;
-        int math, sci, eng, comp;
-          
-        System.out.print("Enter your complete name: ");
+
+        System.out.print("Enter student ID: ");
+        id = input.nextLine();
+        System.out.print("Enter your full name: ");
         name = input.nextLine();
-        
-        System.out.println("Enter your Marks");
-        System.out.print("\nMath:");
+        System.out.print("Enter Marks in:\n");
+        System.out.print("Math: ");
         math = input.nextInt();
-        System.out.print("Science:");     
+        System.out.print("Science: ");
         sci = input.nextInt();
-        System.out.print("English:");
+        System.out.print("English: ");
         eng = input.nextInt();
-        System.out.print("Computer:");
-        comp = input.nextInt();
-        
-        int totalMarks = math + sci + eng + comp;
+        System.out.print("Computer: ");
+        com = input.nextInt();
+
+        printGradeDetails();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void printGradeDetails() {
+        int totalMarks = math + sci + eng + com;
         double averageMarks = totalMarks / 4.0;
-        
+
+        System.out.println("-------------------");
         System.out.println("Grade Detail");
-        
-        System.out.println("Your complete name:"+name);
-        System.out.println("Total Marks:"+totalMarks);
-        System.out.println("Total Average"+averageMarks);
-        
+        System.out.println("-------------------");
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.println("Total Average: " + averageMarks);
+        System.out.println("-------------------");
+    }
+
+    public void editGrade() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Edit Marks for " + name);
+        System.out.print("Enter new marks in Math: ");
+        math = input.nextInt();
+        System.out.print("Enter new marks in Science: ");
+        sci = input.nextInt();
+        System.out.print("Enter new marks in English: ");
+        eng = input.nextInt();
+        System.out.print("Enter new marks in Computer: ");
+        com = input.nextInt();
+
+        System.out.println("Grades updated successfully.");
+        printGradeDetails();
+    }
+
+    public void deleteGrade() {
+        name = "";
+        math = sci = eng = com = 0;
+
+        System.out.println("Grade records deleted successfully.");
+    }
+}
